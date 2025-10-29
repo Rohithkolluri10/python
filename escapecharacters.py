5bb9bb3d3ac641637a20e85b5c392d3ead4e265a
Rohith Kolluri <rohit-git@gmail.com>
2025-10-29 12:10:03 +0530

    Escape Characters in Strings

---
+++ escapecharacters.py
@@ -0,0 +1,15 @@
+# Escape characters in Strings are used to represent special characters within string literals.
+
+# Escape characters are preceded by a backslash (\)
+#  to indicate that the following character has a special meaning.
+
+# Common escape characters in Python:
+# \n : Newline
+# \t : Tab
+# \\ : Backslash
+# \' : Single Quote
+# \" : Double Quote
+print("Hello,\nWorld!")  # Newline
+print("Hello,\tWorld!")  # Tab  
+print("This is a backslash: \\")  # Backslash
+print('It\'s a beautiful day!')  # Single Quote
