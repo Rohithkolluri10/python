f8e41ab5d0179992f37c10a2b6821fc5b95845e7
Rohith Kolluri <rohit-git@gmail.com>
2025-10-30 16:52:46 +0530

    Collections List has been added

---
+++ lists.py
@@ -0,0 +1,26 @@
+# Collections in python: Lists
+
+# A list is a collection which is ordered and changeable. Allows duplicate members.
+
+# creating a list
+
+mylist = ["apple", "banana","cherry"]
+print(mylist)
+
+# Accessing the items in the list
+
+# indexing starts from 0
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+# negative indexing
+print(mylist[-1])  # last item
+print(mylist[-2])  # second last item
+print(mylist[-3])  # third last item
+
+# Range of indexes
+print(mylist[1:3])  # from index 1 to index 2
+print(mylist[:2])   # from start to index 1
+print(mylist[1:])   # from index 1 to end
+
